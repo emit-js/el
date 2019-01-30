81221498d5bec1769ed4dfb58090679772eaccf9
winton <mail@wintoni.us>
2019-01-29 23:51:21 -0500

    First commit

---
+++ el.js
@@ -0,0 +1,90 @@
+/*global document*/
+var htmlProps = {
+  className: true,
+  id: true,
+  innerHTML: true,
+  nodeValue: true,
+  tabIndex: true,
+  textContent: true,
+  value: true,
+}
+
+module.exports = function(dot) {
+  if (dot.state.el) {
+    return
+  }
+
+  dot.state.el = { events: {} }
+  dot.el = el.bind(dot.state.el)
+}
+
+function el(tagName) {
+  var node =
+    tagName.nodeType === 1
+      ? tagName
+      : document.createElement(tagName)
+
+  for (var i = 1; i < arguments.length; ++i) {
+    var arg = arguments[i]
+    if (arg != null) {
+      if (!arg.constructor || arg.constructor === Object) {
+        for (
+          var j = 0, ks = Object.keys(arg);
+          j < ks.length;
+          ++j
+        ) {
+          var key = ks[j],
+            val = arg[key]
+          if (key === "style") {
+            node.style.cssText = val
+          } else if (
+            typeof val !== "string" ||
+            htmlProps[key]
+          ) {
+            node[key] = val
+            //set synthetic events for onUpperCaseName
+            if (
+              key[0] === "o" &&
+              key[1] === "n" &&
+              key.charCodeAt(2) < 91 &&
+              key.charCodeAt(2) > 64 &&
+              !this.events[key]
+            ) {
+              document.addEventListener(
+                key.slice(2).toLowerCase(),
+                function(e) {
+                  var tgt = e.target
+                  do {
+                    if (tgt[key]) {
+                      return tgt[key](e)
+                    }
+                  } while ((tgt = tgt.parentNode))
+                }
+              )
+              this.events[key] = true
+            }
+          } else {
+            node.setAttribute(key, val)
+          }
+        }
+      } else {
+        if (Array.isArray(arg)) {
+          for (var k = 0; k < arg.length; ++k) {
+            node.appendChild(
+              arg[k].nodeType
+                ? arg[k]
+                : document.createTextNode(arg[k])
+            )
+          }
+        } else {
+          node.appendChild(
+            arg.nodeType
+              ? arg
+              : document.createTextNode(arg)
+          )
+        }
+      }
+    }
+  }
+  return node
+}
---
+++ elTest.js
@@ -0,0 +1,128 @@
+/* global document */
+/* eslint-env jest */
+
+var dot = require("dot-event")(),
+  JSDOM = require("jsdom").JSDOM
+
+var window = new JSDOM().window
+
+require("./")(dot)
+var el = dot.el
+
+global.document = window.document
+
+test("element - nodeType", function() {
+  expect(el("div").nodeType).toBe(1)
+  expect(el("p").nodeType).toBe(1)
+  expect(el(document.createElement("p")).nodeType).toBe(1)
+})
+
+test("element - properties", function() {
+  expect(el("div").attributes.length).toBe(0)
+  expect(el("div", { id: "id" }).id).toBe("id")
+  expect(
+    el("div", { className: "className" }).className
+  ).toBe("className")
+  expect(el("input", { value: "value" }).value).toBe(
+    "value"
+  )
+  expect(
+    el("p", { textContent: "textContent" }).textContent
+  ).toBe("textContent")
+})
+
+test("element - attributes", function() {
+  expect(el("div").attributes.length).toBe(0)
+  expect(
+    el("div", { class: "class" }).attributes.length
+  ).toBe(1)
+  expect(
+    el("div", { style: "style" }).attributes.length
+  ).toBe(1)
+  expect(
+    el("div", { "data-set": "data-set" }).attributes.length
+  ).toBe(1)
+  expect(
+    el("div", { value: "value" }).attributes.length
+  ).toBe(0)
+})
+
+test("element - mixed children", function() {
+  expect(
+    el("p", [0, el("p"), el("p"), el("p")]).childNodes
+      .length
+  ).toBe(4)
+  expect(
+    el("p", el("p"), [], el("p"), [el("p"), 0]).childNodes
+      .length
+  ).toBe(4)
+  expect(
+    el("p", [el("p"), 0, [el("p"), el("p")]]).childNodes
+      .length
+  ).toBe(3)
+})
+
+test("el - event", function() {
+  var kin = el("h1", {
+    onclick: function(e) {
+      this.textContent += e.target.tagName
+    },
+  })
+  kin.dispatchEvent(
+    new window.Event("click", { bubbles: true })
+  )
+  expect(kin.textContent).toBe("H1")
+})
+
+test("el - synthetic event", function() {
+  var h2 = el("h2")
+  var h1 = el(
+    "h1",
+    {
+      onClick: function(e) {
+        this.textContent = e.target.tagName
+      },
+    },
+    h2
+  )
+  document.body.appendChild(h1)
+  h1.dispatchEvent(
+    new window.Event("click", { bubbles: false })
+  )
+  expect(h1.textContent).toBe("")
+  h2.dispatchEvent(
+    new window.Event("click", { bubbles: true })
+  )
+  expect(h1.textContent).toBe("H2")
+  h1.dispatchEvent(
+    new window.Event("click", { bubbles: true })
+  )
+  expect(h1.textContent).toBe("H1")
+})
+
+test("element - update", function() {
+  var kin = el("span", "b", {
+    update: function(v) {
+      this.textContent = v.toUpperCase()
+    },
+  })
+  expect(kin.textContent).toBe("b")
+
+  kin.update("abc")
+  expect(kin.textContent).toBe("ABC")
+})
+
+test("element - nested reference", function() {
+  var kid = el("span", "b"),
+    kin = el("h1", el("h2", el("h3", el("h4", kid))), {
+      __kid: kid,
+      update: function(v) {
+        this.__kid.textContent = v
+      },
+    })
+
+  expect(kid.textContent).toBe("b")
+
+  kin.update("B")
+  expect(kid.textContent).toBe("B")
+})
